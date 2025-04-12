f64012cdc0e0ac742652544bcb72a9a5033c301f
IIIWA <72808562+IIIWA@users.noreply.github.com>
2025-04-12 20:24:12 +0300

    add 94, 101, 103

---
+++ homework-folder/lesson103/script.js
@@ -0,0 +1,73 @@
+let me = {
+    name: 'Ivan',
+    age: 25,
+    weight: 100,
+    city: 'Saint Petersburg',
+    isAdmin: false,
+    sayHello(name) {
+        console.log(`Hello ${name}`)
+    },
+
+}
+
+console.log(me)
+
+me.sayHello('Dorothy')
+
+// ===========================================================>
+
+
+let users = [
+    {
+        name: 'Ivan',
+        age: 25,
+        weight: 100,
+        city: 'Saint Petersburg',
+        isAdmin: false,
+    },
+    {
+        name: 'Sven',
+        age: 25,
+        weight: 100,
+        city: 'Saint Petersburg',
+        isAdmin: false,
+    },
+    {
+        name: 'Seeker',
+        age: 25,
+        weight: 100,
+        city: 'Saint Petersburg',
+        isAdmin: true,
+    },
+    {
+        name: 'Zeus',
+        age: 25,
+        weight: 100,
+        city: 'Saint Petersburg',
+        isAdmin: false,
+    },
+    {
+        name: 'Chaose',
+        age: 25,
+        weight: 100,
+        city: 'Saint Petersburg',
+        isAdmin: true,
+    },
+    {
+        name: 'Nami',
+        age: 25,
+        weight: 100,
+        city: 'Saint Petersburg',
+        isAdmin: false,
+    },
+];
+
+let simpleUser = 0;
+
+for(let i = 0; i < users.length; i++){
+    if(users[i].isAdmin == false){
+        simpleUser++
+    }
+}
+
+console.log(simpleUser);
---
+++ homework-folder/lesson94/script.js
@@ -0,0 +1,11 @@
+let userName = 'Ivan';
+let userSureName = 'Gavriliuk';
+let userAge = 25;
+let userWeight = 100; 
+let userMood = 'excellent';
+
+console.log(userName);
+console.log(userSureName);
+console.log(userAge);
+console.log(userWeight);
+console.log(userMood);
